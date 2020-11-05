f91f93b8c9761a9471f9a8dedcfb3c9204f1e43e
Raul Nuñez Sebastian <raul.nunseb@educa.jcyl.es>
2020-11-05 18:05:07 +0100

    Add proyectoDWES

---
+++ indexProyectoDWES.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+	<html lang="es">
+		<head>
+			<meta charset="UTF-8">
+			<title>Raúl Núñez Sebastián</title>
+			<link rel="stylesheet" href="webroot/css/estilos.css">
+			<link rel="stylesheet" href="webroot/css/fontello.css">
+			<link rel="stylesheet" href="webroot/css/menu.css">
+			<link rel="stylesheet" href="webroot/css/banner.css">
+			<link rel="stylesheet" href="webroot/css/modulos.css">
+		</head>
+		<body>
+			<header>
+				<div class="contenedor">
+					<h1 class="icon-rocket">Desarrollo de Aplicaciones Web</h1>
+					<input type="checkbox" id="menu-bar">
+					<label class="icon-menu" for="menu-bar"></label>
+						<nav class="menu">
+							<a href="../index.html">Inicio</a>
+							<a href="proyectoDWES/indexProyectoDWES.php">DWES</a>
+							<a href="">DWEC</a>
+							<a href="">DAW</a>
+							<a href="">DIW</a>
+						</nav>
+				</div>
+			</header>
+			<main>
+				<section id="banner">
+					<img src="webroot/media/banner.jpg" alt="banner">
+					<div class="contenedor">
+						<h2>Proyecto Web De</h2>
+						<p>Raúl Núñez Sebastián</p>
+					</div>
+				</section>
+				<section id="bienvenidos">
+					<div class="contenedor">
+                                            <a target="blanc" href="doc/201002_EjerciciosTema1_Raul.pdf"><h3>Tema 1: Desarrollo Web en Entorno Servidor.</h3></a>
+                                            <a target="blanc" href="doc/201008_EjerciciosTema2_Raul.pdf"><h3>Tema 2: Instalación, configuración y documentación del ED y del EE.</h3></a>
+                                            <a href="../proyectoTema3/indexProyectoTema3.php"><h3>Tema 3: Características del lenguaje de PHP.</h3></a>
+                                            <a href="../proyectoTema4/indexProyectoTema4.php"><h3>Tema 4: Técnicas de acceso a datos en PHP.</h3></a>
+					</div>
+				</section>
+			<footer>
+				<div class="contenedor">
+					<p class="copy">Raúl Núñez Sebastián &copy; 2020/2021</p>
+						<div class="sociales">
+							<a class="icon-github" target="blanc" href="https://github.com/RaulNSSauces"></a>
+							<a class="icon-doc-text" target="blanc" href="webroot/curriculum.html"></a>
+							<a class="icon-linkedin" target="blanc" href="https://www.linkedin.com/login/es"></a>
+						</div>
+				</div>
+			</footer>
+		</body>
+	</html>
